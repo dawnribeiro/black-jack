b48b744640afadb383790db12e06a29147b1599d
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-09 16:26:13 -0400

    draw a card now works

--- public/main.js
+++ public/main.js
@@ -1,19 +1,62 @@
 const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-const ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+const ranks = [{
+  rank: 'Ace',
+  value: 11
+}, {
+  rank: '2',
+  value: 2
+}, {
+  rank: '3',
+  value: 3
+}, {
+  rank: '4',
+  value: 4
+}, {
+  rank: '5',
+  value: 5
+}, {
+  rank: '6',
+  value: 6
+}, {
+  rank: '7',
+  value: 7
+}, {
+  rank: '8',
+  value: 8
+}, {
+  rank: '9',
+  value: 9
+}, {
+  rank: '10',
+  value: 10
+}, {
+  rank: 'Jack',
+  value: 10
+}, {
+  rank: 'Queen',
+  value: 10
+}, {
+  rank: 'King',
+  value: 10
+}]
 
 const deck = []
 
-const main = () => {
-  for (let i = 0; i < suits.length; i++) {
-    const suit = suits[i]
-    for (let j = 0; j < ranks.length; j++) {
-      const rank = ranks[j]
-      const card = rank + ' ' + suit
+const createDeck = () => {
+  for (let j = 0; j < ranks.length; j++) {
+    const rank = ranks[j]
+    for (let i = 0; i < suits.length; i++) {
+      const suit = suits[i]
+      console.log(rank)
+      console.log(suit)
+      const card = {
+        value: rank.value,
+        rank: rank.rank,
+        suit: suit
+      }
       deck.push(card)
     }
   }
-  shuffle(deck)
-  console.log(deck)
 }
 const shuffle = () => {
   for (let i = deck.length - 1; i > 0; i--) {
@@ -21,8 +64,22 @@ const shuffle = () => {
     let temp = deck[i]
     deck[i] = deck[randomPosition]
     deck[randomPosition] = temp
-
   }
 }
-// document.querySelector('.draw-btn').addEventListener('click', cardDrawn)
+const main = () => {
+  createDeck()
+  shuffle()
+}
+const cardDrawn = () => {
+  const firstCard = deck[0]
+  console.log(firstCard)
+  document.querySelector('.card-drawn').textContent =
+    firstCard.rank +
+    ' of ' +
+    firstCard.suit +
+    ' has a value of ' +
+    firstCard.value
+}
+
+document.querySelector('.draw-btn').addEventListener('click', cardDrawn)
 document.addEventListener('DOMContentLoaded', main)
