b3659283e8fe19217aa2c541a7af70a94e7a8149
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-09 14:39:39 -0400

    back to earlier shuffle

--- public/main.js
+++ public/main.js
@@ -1,52 +1,9 @@
-// when the page loads, shuffle the deck
-// when user clicks the button, display the first card
-
 const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-const ranks = [{
-  rank: 'Ace',
-  value:
-} {
-  rank: '2',
-  value:
-} {
-  rank: '3',
-  value:
-} {
-  rank: '4',
-  value:
-} {
-  rank: '5',
-  value:
-} {
-  rank: '6',
-  value:
-} {
-  rank: '7',
-  value:
-} {
-  rank: '8',
-  value:
-} {
-  rank: '9',
-  value:
-} {
-  rank: '10',
-  value:
-} {
-  rank: 'Jack',
-  value:
-} {
-  rank: 'Queen',
-  value:
-} {
-  rank: 'King',
-  value:
-}]
+const ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
 const deck = []
 
-let shuffledDeck = () => {
-  console.log('page loaded')
+const main = () => {
   for (let i = 0; i < suits.length; i++) {
     const suit = suits[i]
     for (let j = 0; j < ranks.length; j++) {
@@ -55,7 +12,6 @@ let shuffledDeck = () => {
       deck.push(card)
     }
   }
-
   shuffle(deck)
   console.log(deck)
 }
@@ -65,33 +21,8 @@ const shuffle = () => {
     let temp = deck[i]
     deck[i] = deck[randomPosition]
     deck[randomPosition] = temp
+
   }
 }
-const drawCard = () => {
-  console.log('card drawn')
-  deck[drawnCard]
-  if (drawCard) {
-    firstCard += 1
-  }
-
-}
-
-const main = () => {
-  shuffledDeck()
-}
-
-
-
-document.querySelector('.draw-btn').addEventListener('click', drawCard)
-
+// document.querySelector('.draw-btn').addEventListener('click', cardDrawn)
 document.addEventListener('DOMContentLoaded', main)
-
-/*
-const card={
-  rank: ranks[j]
-  suit: suits[i]
-  value: cardValues
-  
-
-        get face
-        push card to array
